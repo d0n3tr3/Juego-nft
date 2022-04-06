bcc61e67680242a29f7915783c915f25b4e9f282
F3lipe4 <sdorregos@esliceu.net>
2022-04-06 11:26:50 +0200

    333

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include 'includes/header.php';
 
 <body>
     <header>
-		<h1>Sobre Nosotros
+		<h1>Sobre Nosotros 333</h1>
 		<h3>¿Qué somos?</h3>
 		<h5>Somo una empresa que se dedica a la creación de videojuegos NFT Play to earn(Jugar para ganar)</h5>
 
