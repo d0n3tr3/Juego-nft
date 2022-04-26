8b030f9b8d0e3c3e080779d1797c964a57d38c39
d0netr3 <dlopezm@esliceu.net>
2022-04-26 10:31:32 +0200

    login structure changes

--- log in.php
+++ log in.php
@@ -19,7 +19,7 @@ include 'includes/header.php';
                         <form>
                             <input type="text" minlength="1" maxlength="20" required placeholder="Nombre" />
                             <input type="text" minlength="1" maxlength="30" required placeholder="Apellidos" />
-                           <input type="text" placeholder="DIN" />
+                            <input type="text" placeholder="DIN" />
                             <input type="date" required placeholder="Fecha" />
                             <input type="email" minlenght="11" maxlength="40" required placeholder="Email" />
                             <input type="tel" min="0" max="9" placeholder="tel." />
@@ -27,8 +27,8 @@ include 'includes/header.php';
                         </form>
                     </tbody>
                     </thead>
-                    </tableborder>
-                    </sellection>
+                </tableborder>
+            </sellection>
         </section>
 </body>
 
