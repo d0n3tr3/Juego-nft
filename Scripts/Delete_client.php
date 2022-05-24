1fd60d33704506878a5d1584550077d39718959d
F3lipe4 <sdorregos@esliceu.net>
2022-05-24 10:40:23 +0200

    Inserts, deletes i updates

--- Scripts/Delete_client.php
+++ Scripts/Delete_client.php
@@ -4,11 +4,11 @@ include '../includes/database.php';
 $Gmail = $_POST['Gmail'];
 $pais = $_POST['pais'];
 $cp = $_POST['cp'];
-$nom = $_POST['nom'];
-$id = $_POST['id'];
+$nom_client = $_POST['nom'];
+$idusuari = $_POST['idusuari'];
 $DNI = $_POST['DNI'];
 
-$query = "DELETE * FROM clients WHERE id = '$id' ";
+$query = "DELETE * FROM clients WHERE idusuari = '$idusuari' ";
 
 $result = mysqli_query($dbh, $query);
 
---
+++ Scripts/Insert_modedejoc.php
@@ -0,0 +1,17 @@
+<?php
+include '../includes/database.php'
+
+
+$Dificultat = $_POST['dificultat'];
+$Ganancias = $_POST['Ganancias'];
+
+
+$query = "INSERT INTO mode_de_joc (dificultat, Gananacias) VALUES('$Dificultat', '$Ganancias')";
+
+$result = mysqli_query($dbh, $query);
+
+if($result){
+    header('Location: ../mode_de_joc.php');
+}else{
+    echo mysqli_error($dbh);
+}
