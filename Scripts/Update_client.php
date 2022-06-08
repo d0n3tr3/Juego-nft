26fd26ffad416ff41b3787e5dd920a9ae8767568
F3lipe4 <sdorregos@esliceu.net>
2022-06-08 09:26:26 +0200

    log de clientes arreglado

--- Scripts/Update_client.php
+++ Scripts/Update_client.php
@@ -4,17 +4,17 @@ include '../includes/database.php';
 $Gmail = $_POST['Gmail'];
 $pais = $_POST['pais'];
 $cp = $_POST['cp'];
-$nom_client = $_POST['nom'];
+$nom= $_POST['nom'];
 $Pasw = $_POST['Pasw'];
 $idusuari = $_POST['idusuari'];
 $DNI = $_POST['DNI'];
 
-$query = "UPDATE usuari SET nom = '$nom_client', Gmail = '$Gmail',Pasw ='$Pasw', cp = '$cp', pais = '$pais' WHERE idusuari = '$idusuari' ";
-echo $query;
+$query = "UPDATE usuari SET nom = '$nom', Gmail = '$Gmail',Pasw ='$Pasw', cp = '$cp', pais = '$pais' WHERE idusuari = '$idusuari' ";
+
 $result = mysqli_query($dbh, $query);
 
 if($result){
-   // header('Location: ../clientes.php');
+   header('Location: ../clientes.php');
 }else{
     echo mysqli_error($dbh);
 }
