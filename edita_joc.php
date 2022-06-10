5159a7e579a889a134169f975f2b4247f99481ea
F3lipe4 <sdorregos@esliceu.net>
2022-06-10 08:35:17 +0200

    areglosfinales de editar

--- edita_joc.php
+++ edita_joc.php
@@ -18,7 +18,7 @@ if(isset($_GET["id"])){
 }
 
 
-$action = "Scripts/insert_JOC.php";
+$action = "Scripts/Insert_JOC.php";
 if($idjoc != null){
   $action ="Scripts/Update_JOC.php";
 }
@@ -42,12 +42,12 @@ if($idjoc != null){
     <input type="hidden" name="idjoc" value="<?=$JOC['idjoc']?>"/> 
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Nombre</label>
-    <input value="<?=$JOC['nom'];?>" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input name="nom" value="<?=$JOC['nom'];?>" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
     <div id="emailHelp"  class="form-text"> </div>
   </div>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Token</label>
-    <input value="<?=$JOC['token'];?>" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input name="token" value="<?=$JOC['token'];?>" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
     <div id="emailHelp"  class="form-text"> </div>
   </div>
   <div class="mb-3 form-check">
