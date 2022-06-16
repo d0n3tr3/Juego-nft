258c933156756033df488a471ea0bd8835b8f3a9
F3lipe4 <sdorregos@esliceu.net>
2022-06-16 23:49:47 +0200

    mejoras

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <!DOCTYPE html>
 <html>
+<body>
 <?php
 include 'includes/head.php';
-?>
 
-<?php
 include 'includes/header.php';
 ?>
 
 
 <div class="mb-3"></div>
 <div class="container"></div>
-<body>
-		<h1>Sobre Nustra empresa</h1>
-		<div class="mt-3"></div>
-		<h3>¿Qué somos?</h3>
-		<h5>Somo una empresa que se dedica a...</h5>
-			<div class="mb-3"></div>
-		<h6> Para crear una cuenta utiliza el siguiente boton</h6>
+<h1>Sobre Nustra empresa</h1>
+<div class="mt-3"></div>
+<h3>¿Qué somos?</h3>
+<h5>Somo una empresa que se dedica a la creación de videojuegos con recompensas NFT</h5>
+<h5> </h5>
+<div class="mb-3"></div><h6> Para crear una cuenta utiliza el siguiente boton</h6>
+
 <a class='btn btn-primary' href="log_in.php?id=">Log in</a>
 
-		</body>
+</body>
 </html>
